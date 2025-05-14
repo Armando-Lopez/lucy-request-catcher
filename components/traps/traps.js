dc2d8ed0a0ce91b2452a4b9c250fa9308e2500ac
armando-lopez <lopexdiego5@gmail.com>
2025-05-14 16:30:44 -0500

    feat: support mustache sintax

--- components/traps/traps.js
+++ components/traps/traps.js
@@ -7,7 +7,7 @@ export function registerTrapsComponent() {
     template: html`
       <div>
         <template l-for="trap in traps">
-          <trap-item-component p:trap="trap"></trap-item-component>
+          <trap-item-component l:trap="trap"></trap-item-component>
         </template>
       </div>
     `,
@@ -32,7 +32,13 @@ export function registerTrapsComponent() {
         <div class="flex items-center gap-1">
           <span>{{ trap.method }}</span>
           -
-          <span class="text-green-600">{{ trap.statusCode }}</span>
+          <span
+            l:class="{
+              isSuccessCode => text-green-600,
+              isErrorCode => text-red-600
+            }"
+            >{{ trap.statusCode }}</span
+          >
           <div class="ml-auto space-x-2">
             <button class="px-2 py-1 text-white rounded-md bg-gray-700">
               Capturar
@@ -55,7 +61,10 @@ export function registerTrapsComponent() {
     `,
     props: ["trap"],
     data() {
+      const isSuccessCode = this.$props.trap.statusCode < 400;
       return {
+        isSuccessCode,
+        isErrorCode: !isSuccessCode,
         response: JSON.stringify(this.$props.trap.response, null, 2),
       };
     },
@@ -63,7 +72,7 @@ export function registerTrapsComponent() {
       console.log("Mounted child");
       // this.traps = ["a", "b", "c", "d"];
       // console.log(this.$data);
-      console.log(this.$props.trap.response);
+      // console.log(this.$props.trap.response);
     },
     onUpdated(data) {
       console.log("Updated child", data);
